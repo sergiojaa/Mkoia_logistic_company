eefc7151f2da88fa0e3f3b774211e7c5f5036e5d
seriojaa <kaliashvilisergi@gmail.com>
2025-04-06 19:04:46 +0400

    make responsive for titles and description

--- app/components/home/Trust.tsx
+++ app/components/home/Trust.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Trust() {
     return (
-        <div>
-            trust
+        <div className='max-w-[1200px] mx-auto'>
+            <div className='flex flex-col gap-5 items-start p-5 '>
+                <h3 className='text-[#2A437C]'>TESTIMONIALS</h3>
+                <div className='lg:flex items-center gap-56  '>
+                    <h1 className='text-[#001E62] text-3xl font-bold'> <span className='text-[#C8102E]'>Trusted</span> by Worldwide</h1>
+                    <p className='text-[#2A437C] max-w-[620px] text-lg'>Many companies already work with us. Praise pleases us, criticism helps us because we want to get better every day.</p>
+                </div>
+
+            </div>
         </div>
     )
 }
