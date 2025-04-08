14eb468f291d0270e415657ba4e65dc133fa1479
seriojaa <kaliashvilisergi@gmail.com>
2025-04-08 19:53:34 +0400

    Add desktop and mobile images

--- app/components/about/History.tsx
+++ app/components/about/History.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function History() {
     return (
         <div>
-            history
+            <div>
+                <Image
+                    className='hidden md:block'
+                    src='/about/History-desktop.png' width={1200} height={200} alt='image' />
+                <Image
+                    className='md:hidden'
+                    src='/about/History-mobile.png' width={1200} height={200} alt='image' />
+
+            </div>
         </div>
     )
 }
