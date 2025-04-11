0b5a7c3fe3a1499a9b280374f1b7de90b50d3562
seriojaa <kaliashvilisergi@gmail.com>
2025-04-11 11:23:15 +0400

    CHange images

--- app/components/services/Servicesimage.tsx
+++ app/components/services/Servicesimage.tsx
@@ -3,14 +3,19 @@ import Image from 'next/image'
 export default function Servicesimage() {
     return (
         <div className='w-full p-5'>
-            <div>
-                <Image src='/services/first-image.png' alt='logistic image' width={1200} height={100} />
+            <div className='relative'>
+                <div>
+                    <Image className='md:hidden' src='/services/image-mobile.png' alt='logistic image' width={1200} height={100} />
+                    <Image className='hidden md:block' src='/services/image-desktop.png' alt='logistic image' width={1200} height={100} />
+
+                </div>
+                <div className='absolute text-center text-white top-10 flex flex-col items-center justify-center'>
+                    <h2 className='font-bolt text-[40px] '>Logistics Services</h2>
+                    <p>Our comprehensive logistics solution is designed to optimize your supply chain and enhance your business operations.</p>
+                    <p>CUSTOM SOLUTIONS FOR EVERY NEED</p>
+                </div>
             </div>
-            <div>
-                <h2>Logistics Services</h2>
-                <p>Our comprehensive logistics solution is designed to optimize your supply chain and enhance your business operations.</p>
-                <p>CUSTOM SOLUTIONS FOR EVERY NEED</p>
-            </div>
+
         </div>
     )
 }
