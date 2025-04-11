4cb524ccf9f942c78604d34e91205b28550ca5e6
seriojaa <kaliashvilisergi@gmail.com>
2025-04-11 14:23:52 +0400

    Add community component

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Servicesimage from '../components/services/Servicesimage'
+import Community from '../components/services/Community'
 
 export default function page() {
     return (
         <div className='max-w-[1200px] mx-auto'>
             <Servicesimage />
+            <Community />
         </div>
     )
 }
