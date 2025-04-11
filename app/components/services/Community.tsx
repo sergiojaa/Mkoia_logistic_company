9d117567cf747284ae4eb7d3abd43d931e602914
seriojaa <kaliashvilisergi@gmail.com>
2025-04-11 14:30:15 +0400

    Add business texts

--- app/components/services/Community.tsx
+++ app/components/services/Community.tsx
@@ -1,9 +1,21 @@
+import { ArrowRight } from 'lucide-react'
 import React from 'react'
 
 export default function Community() {
     return (
-        <div>
-            community
+        <div className='w-full p-5'>
+            <div className="flex items-center">
+                <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
+                <p className=" text-[#2A437C]   ">OUR COMMUNITY</p>
+            </div>
+            <div>
+                <h2 className='text-[#001E62] '>Join a <span className='text-[#D13851]'>Growing Community</span> of Business and Logistics Partners </h2>
+                <p className='text-[#2A437C]'>Join a dynamic community of businesses and logistics partners.optimizing supply chains and driving growth</p>
+                <button className='flex items-center'>
+                    Join Us
+                    <ArrowRight />
+                </button>
+            </div>
         </div>
     )
 }
