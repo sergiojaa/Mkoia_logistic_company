ad318484fa4cd5b705e6fb8294609891e7370e68
seriojaa <kaliashvilisergi@gmail.com>
2025-04-12 21:13:50 +0400

    Add service component

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Servicesimage from '../components/services/Servicesimage'
 import Community from '../components/services/Community'
+import Service from '../components/services/Service'
 
 export default function page() {
     return (
         <div className='max-w-[1200px] mx-auto'>
             <Servicesimage />
             <Community />
+            <Service />
         </div>
     )
 }
