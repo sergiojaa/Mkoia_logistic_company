f757c6004eba128863db5334e61ca9441ba2b776
seriojaa <kaliashvilisergi@gmail.com>
2025-04-12 23:22:21 +0400

    Add texts design

--- app/components/services/Process.tsx
+++ app/components/services/Process.tsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function Process() {
     return (
-        <div>
-            process
-        </div>
+        <div className='w-full mt-10  mx-auto px-7'>
+            <div className='flex lg:items-start  flex-col gap-7 justify-center items-center'>
+                <div className="flex   items-center">
+                    <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
+                    <p className=" text-[#2A437C] text-md   ">Work Process</p>
+                </div>
+                <div >
+                    <h2 className='text-[#001E62] text-2xl lg:text-3xl mx-auto max-w-[500px] text-center'>Our <span className='text-[#D13851]'>Best Process </span> for Excellence</h2>
+                </div>
+                <div>
+                    <p className='text-[#556996] max-w-[500px]'>Our process is simple yet effective. Every project is different, but we’ve seen thousands of them since we first launched. Our experience is your asset.</p>
+
+                </div>
+            </div>
+
+        </div >
     )
 }
