4ff3ef3526888a31a7d1768517ec3eb31ea3fbb1
seriojaa <kaliashvilisergi@gmail.com>
2025-04-12 22:21:38 +0400

    texts design complete

--- app/components/services/Service.tsx
+++ app/components/services/Service.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 export default function Service() {
     return (
-        <div className='w-full mx-auto px-7 '>
-            <div className='flex  flex-col gap-2 justify-center items-center'>
+        <div className='w-full mx-auto mt-10 px-7 '>
+            <div className='flex  flex-col gap-7 justify-center items-center'>
                 <div className="flex  items-center">
                     <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
                     <p className=" text-[#2A437C] text-md   ">Logistic Services</p>
                 </div>
-                <div>
-                    <p className='text-[#001E62] text-2xl lg:text-3xl max-w-[600px] text-center'>Comprehensive <span className='text-[#D13851]'>Logix Services</span>  Tailored to Meet Your Unique <span className='text-[#D13851]'>Needs</span> </p>
+                <div className='flex flex-col items-center gap-6'>
+                    <p className='text-[#001E62] text-2xl lg:text-3xl mx-auto max-w-[600px] text-center'>Comprehensive <span className='text-[#D13851]'>Logix Services</span>  Tailored to Meet Your Unique <span className='text-[#D13851]'>Needs</span> </p>
+                    <p className='text-[#2A437C] text-center mx-auto max-w-[800px]'>Our vision is to be the leading logistics partner, known for excellence in supply chain management, innovation, and customer satisfaction, while exceeding expectations and driving success for our clients globally.</p>
                 </div>
             </div>
 
