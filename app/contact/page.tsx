bd248fa43846f1e4b0f05acf09fa4f539b3c6a14
seriojaa <kaliashvilisergi@gmail.com>
2025-04-13 17:27:07 +0400

    fix gaps

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/contact/Banner'
+import ContactInfo from '../components/contact/ContactInfo'
 export default function page() {
     return (
         <div className='max-w-[1200px] px-7 mt-7 mx-auto'>
             <div>
                 <Banner />
+                <ContactInfo />
             </div>
 
         </div>
