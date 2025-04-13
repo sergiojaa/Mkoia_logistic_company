f0c173f78dc7fcac36937533daede3ec59c5746d
seriojaa <kaliashvilisergi@gmail.com>
2025-04-13 16:53:38 +0400

    Add imagesand text

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,6 +6,14 @@ export default function page() {
             <div>
                 <Banner />
             </div>
+            <div>
+                <h2>Get In Touch with Mkoia</h2>
+                <p>we're here to support your logistixs needs. Reach out to us for inquires, quotes, or assistance</p>
+            </div>
+            <div className="flex items-center">
+                <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
+                <p className="text-xs md:text-sm font-medium tracking-wider">POWERING BUSINESSES GLOBALLY</p>
+            </div>
         </div>
     )
 }
