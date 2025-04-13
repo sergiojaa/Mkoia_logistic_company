ddf58a2c374b048c597858da4de1fbf0be068f66
seriojaa <kaliashvilisergi@gmail.com>
2025-04-13 17:16:43 +0400

    complete sizes

--- app/components/contact/Banner.tsx
+++ app/components/contact/Banner.tsx
@@ -6,11 +6,18 @@ export default function Banner() {
             <div className='relative'>
                 <div>
                     <Image
+                        className="md:hidden w-full h-[370px] object-cover brightness-[0.7]"
+                        src="/contact/banner-mobile.png"
+                        alt="contact image about logistic"
+                        width={1200}
+                        height={370}
+                    />
+                    <Image
                         className="hidden md:block w-full object-cover "
                         src='/contact/banner-desktop.png' alt='contact image about logistic' width={1200} height={500} />
                 </div>
                 <div className="absolute inset-0 text-center text-white flex flex-col items-center justify-center px-4">
-                    <h2 className='font-bold text-3xl md:text-5xl  mb-4'>Get In Touch with Mkoia</h2>
+                    <h2 className='font-bold text-3xl lg:text-4xl  mb-4'>Get In Touch with Mkoia</h2>
                     <p className="max-w-xl text-sm md:text-lg mb-6 ">we're here to support your logistixs needs. Reach out to us for inquires, quotes, or assistance</p>
                     <div className="flex items-center">
                         <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
