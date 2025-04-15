2886783bf0b7e01dc210e6e288e852f577f62007
seriojaa <kaliashvilisergi@gmail.com>
2025-04-15 15:36:34 +0400

    add arrows in faq

--- app/components/contact/Question.tsx
+++ app/components/contact/Question.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { ChevronDown, ChevronUp } from 'lucide-react'
+
 export default function Question({ active, setActive, questionNumber, questionObj, }: any) {
     return (
         <div>
             <div className=' border-b flex flex-col gap-3 border-b-[#CCD2DF] py-5'>
-                <p
-                    className='text-[#001E62] cursor-pointer text-xl'
+                <div
                     onClick={() => {
                         if (active === questionNumber) {
                             setActive(0)
@@ -13,13 +14,31 @@ export default function Question({ active, setActive, questionNumber, questionOb
 
                         }
 
-                    }} >
-                    {questionObj.question}
-                </p>
+                    }}
+                    className='flex cursor-pointer items-center justify-between'>
+                    <p
+                        className='text-[#001E62]  text-xl'
+                    >
+                        {questionObj.question}
+                    </p>
+                    <span
+                        onClick={() => setActive(active ? 0 : questionNumber)}
+                        className="cursor-pointer"
+                    >
+                        {active ? (
+                            <ChevronUp className="w-6 h-6 text-[#001E62]" />
+                        ) : (
+                            <ChevronDown className="w-6 h-6 text-[#001E62]" />
+                        )}
+                    </span>
+                </div>
+
+
                 {active === questionNumber ? (
                     <p className='text-[#556996] text-sm'>{questionObj.answer}</p>
 
                 ) : null}
+
 
 
             </div>
