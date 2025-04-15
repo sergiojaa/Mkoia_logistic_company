3256e6e2393126125faa732730d4887ca026c361
seriojaa <kaliashvilisergi@gmail.com>
2025-04-15 14:18:06 +0400

    import useState and set the values

--- app/components/contact/Faq.tsx
+++ app/components/contact/Faq.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 export default function Faq() {
+    const [active, setActive] = useState(0)
+    console.log(active)
     return (
         <div className='max-w-[1200px] mt-20 mx-auto'>
             <div className='flex  flex-col gap-7 justify-center items-center'>
@@ -11,6 +14,26 @@ export default function Faq() {
                 </div>
                 <div className='flex flex-col items-center gap-6'>
                     <p className='text-[#001E62] text-2xl lg:text-3xl mx-auto max-w-[450px] text-center'>Comprehensive <span className='text-[#D13851]'>Questions</span> About Our Services </p>
+                </div>
+                <div>
+                    <p onClick={() => setActive(1)} >question1</p>
+                    <p>answer1</p>
+                </div>
+                <div>
+                    <p onClick={() => setActive(2)}>question2</p>
+                    <p>answer2</p>
+                </div>
+                <div>
+                    <p onClick={() => setActive(3)}>question3</p>
+                    <p>answer3</p>
+                </div>
+                <div>
+                    <p onClick={() => setActive(4)}>question4</p>
+                    <p>answer4</p>
+                </div>
+                <div>
+                    <p onClick={() => setActive(5)}>question5</p>
+                    <p>answer5</p>
                 </div>
             </div>
         </div>
