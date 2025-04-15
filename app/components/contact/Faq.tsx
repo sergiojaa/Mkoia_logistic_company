d0c1206e397b258c2ebed56cf1cda8f1289d2806
seriojaa <kaliashvilisergi@gmail.com>
2025-04-15 14:11:22 +0400

    Add Frequently Asked Question component

---
+++ app/components/contact/Faq.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Faq() {
+    return (
+        <div className='max-w-[1200px] mt-20 mx-auto'>
+            <div className='flex  flex-col gap-7 justify-center items-center'>
+
+                <div className="flex  items-center">
+                    <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
+                    <p className=" text-[#2A437C] text-md   ">FREQUENTLY ASK QUESTION</p>
+                </div>
+                <div className='flex flex-col items-center gap-6'>
+                    <p className='text-[#001E62] text-2xl lg:text-3xl mx-auto max-w-[450px] text-center'>Comprehensive <span className='text-[#D13851]'>Questions</span> About Our Services </p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../components/contact/Banner'
 import ContactInfo from '../components/contact/ContactInfo'
 import MapImage from '../components/contact/MapImage'
+import Faq from '../components/contact/Faq'
 export default function page() {
     return (
         <div className='max-w-[1200px] px-7 mt-7 mx-auto'>
@@ -9,6 +10,7 @@ export default function page() {
                 <Banner />
                 <ContactInfo />
                 <MapImage />
+                <Faq />
             </div>
 
         </div>
