787bca8f6ec3b7bf308e78cd1ec3bb1069252b13
seriojaa <kaliashvilisergi@gmail.com>
2025-04-16 13:50:24 +0400

    created last banner component

---
+++ app/components/contact/LastBanner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LastBanner() {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,7 @@ import Banner from '../components/contact/Banner'
 import ContactInfo from '../components/contact/ContactInfo'
 import MapImage from '../components/contact/MapImage'
 import Faq from '../components/contact/Faq'
+import LastBanner from '../components/contact/LastBanner'
 export default function page() {
     return (
         <div className='max-w-[1200px] px-7 mt-7 mx-auto'>
@@ -11,6 +12,7 @@ export default function page() {
                 <ContactInfo />
                 <MapImage />
                 <Faq />
+                <LastBanner />
             </div>
 
         </div>
