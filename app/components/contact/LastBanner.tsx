48bbbeca8b381654cd57c563d7cd1e6041f8f58a
seriojaa <kaliashvilisergi@gmail.com>
2025-04-16 13:55:39 +0400

    import image

--- app/components/contact/LastBanner.tsx
+++ app/components/contact/LastBanner.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import Image from 'next/image'
 export default function LastBanner() {
     return (
         <div>
+
+            <Image
+                className='md:hidden'
+                src='/contact/lastbanner-mobile.png' width={500} height={300} alt=' logistic company contact image' />
+            <Image
+                className='hidden md:block'
+                src='/contact/lastbanner-desktop.png' width={1200} height={300} alt=' logistic company contact image' />
 
         </div>
     )
