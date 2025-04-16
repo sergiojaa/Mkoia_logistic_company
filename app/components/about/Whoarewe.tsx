80c8745101153d71e5f3a40d59895519f140134d
seriojaa <kaliashvilisergi@gmail.com>
2025-04-16 22:53:24 +0400

    change image sizes

--- app/components/about/Whoarewe.tsx
+++ app/components/about/Whoarewe.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 import Image from 'next/image'
 export default function Whoarewe() {
     return (
-        <div className='w-full p-5'>
+        <div className='w-full mt-10 p-5'>
             <div className='flex flex-col gap-10 justify-center lg:flex-row lg:justify-between items-center'>
-                <div className='flex max-w-[700px] flex-col gap-5'>
+                <div className='flex max-w-[700px] flex-col gap-7'>
                     <div className='flex items-center'>
                         <div className="w-3 h-3 rounded-full bg-red-500 mr-2"></div>
                         <h3>Who are we</h3>
                     </div>
-                    {/* <div className='max-w-[520px]'> */}
                     <h2 className='text-[#001E62] max-w-[520px] text-4xl'>We Specialize in <span className='text-[#D13851]'> End-to-end Logistics Services</span> That Ensure Timely and Secure Delivery</h2>
-                    <p className='text-[#2A437C] max-w-[500px] text-md '>Our dedicated team works around the clock to provide tailored solutions for good businesses of all sizes, helping you meet your goals with ease and reliability. Start from customs brokerage to last-mile delivery, we’re committed to quality and precision in every service we offer. </p>
-                    {/* </div> */}
+                    <p className='text-[#2A437C] max-w-[550px] text-md '>Our dedicated team works around the clock to provide tailored solutions for good businesses of all sizes, helping you meet your goals with ease and reliability. Start from customs brokerage to last-mile delivery, we’re committed to quality and precision in every service we offer. </p>
                     <div className='max-w-[490px] border-b-4 border-b-[#D13851] rounded-xl p-5 bg-[#EEF4FF]'>
                         <h2 className='text-[#001E62] font-bold'>
                             We simplify logistics with advanced technology, so you can focus on business growth
@@ -21,7 +19,7 @@ export default function Whoarewe() {
                 </div>
 
                 <div>
-                    <Image src='/about/containers.png' alt='logistic containers' width={500} height={100} />
+                    <Image src='/about/containers.png' alt='logistic containers' width={400} height={100} />
                 </div>
 
 
