cab55a894278a7f9243cfc6077e492dbab243680
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 03:07:12 +0400

    change texts

--- app/components/contact/Banner.tsx
+++ app/components/contact/Banner.tsx
@@ -17,11 +17,11 @@ export default function Banner() {
                         src='/contact/banner-desktop.png' alt='contact image about logistic' width={1200} height={500} />
                 </div>
                 <div className="absolute inset-0 text-center text-white flex flex-col items-center justify-center px-4">
-                    <h2 className='font-bold text-3xl lg:text-4xl  mb-4'>Get In Touch with Mkoia</h2>
-                    <p className="max-w-xl text-sm md:text-lg mb-6 ">we are here to support your logistixs needs. Reach out to us for inquires, quotes, or assistance</p>
+                    <h2 className='font-bold text-3xl lg:text-4xl  mb-4'>დაუკავშირდით ემკოიას</h2>
+                    <p className="max-w-xl text-sm md:text-lg mb-6 ">ჩვენ აქ ვართ, რათა დავეხმაროთ თქვენს ლოჯისტიკურ საჭიროებებში. დაგვიკავშირდით ნებისმიერი კითხვით.</p>
                     <div className="flex items-center">
                         <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
-                        <p className="text-xs md:text-sm font-medium tracking-wider">POWERING BUSINESSES GLOBALLY</p>
+                        <p className="text-xs md:text-sm font-medium tracking-wider">გლობალური ბიზნესის გაძლიერება</p>
                     </div>
                 </div>
             </div>
