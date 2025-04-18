a019a7d15765397fc06c6feeff88bf696c9b921c
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 03:20:35 +0400

    change texts

--- app/components/contact/LastBanner.tsx
+++ app/components/contact/LastBanner.tsx
@@ -12,10 +12,10 @@ export default function LastBanner() {
                     className='hidden rounded-4xl md:block'
                     src='/contact/lastbanner-desktop.png' width={1200} height={300} alt=' logistic company contact image' />
                 <div className="absolute lg:left-20  inset-0 text-white flex flex-col items-center md:items-start justify-center gap-4 px-4  md:px-10 text-center md:text-left">
-                    <h2 className="font-bold  text-xl  max-w-[650px] lg:text-3xl   mb-4">Contact us to discuss how our services can support your business and streamline your <br /> logistics needs.</h2>
+                    <h2 className="font-bold  text-xl  max-w-[650px] lg:text-xl   mb-4">დაგვიკავშირდით, რათა განვიხილოთ როგორ შეგვიძლია მხარი დავუჭიროთ თქვენს ბიზნესს გაამარტივოს<br /> ლოჯისტიკური საჭიროებები.</h2>
                     <div >
                         <button className="flex max-w-max bg-white text-[#001E62]   cursor-pointer  gap-3 rounded-full    px-6 py-2 transition-colors ">
-                            <span className=''>Contact Us</span>
+                            <span className=''>დაგვიკავშირდი</span>
                             <div className="border bg-[#001E62] text-white  rounded-full p-1  ">
                                 <ArrowRight size={16} />
                             </div>
