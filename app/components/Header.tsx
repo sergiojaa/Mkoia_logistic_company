e13328116fd7a80a1f1acd2f5221467636338f03
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 03:27:49 +0400

    fix eror

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import { ArrowRight, X } from "lucide-react";
 import { usePathname } from "next/navigation";
