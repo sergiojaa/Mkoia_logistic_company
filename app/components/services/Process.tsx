7b0618ee476532f0d8ce60005dcbf19253c0d509
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 03:05:11 +0400

    change text

--- app/components/services/Process.tsx
+++ app/components/services/Process.tsx
@@ -7,14 +7,14 @@ export default function Process() {
             <div className='flex lg:items-start  flex-col gap-7 justify-center items-center'>
                 <div className="flex   items-center">
                     <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
-                    <p className=" text-[#2A437C] text-md   ">Work Process</p>
+                    <p className=" text-[#2A437C] text-md   ">სამუშაო პროცესი</p>
                 </div>
                 <div >
-                    <h2 className='text-[#001E62] text-2xl lg:text-3xl mx-auto max-w-[500px] text-center'>Our <span className='text-[#D13851]'>Best Process </span> for Excellence</h2>
+                    <h2 className='text-[#001E62] text-2xl lg:text-3xl mx-auto max-w-[500px] '>ჩვენი <span className='text-[#D13851]'>საუკეთესო პროცესი </span> სრულყოფილებისთვის</h2>
 
                 </div>
                 <div>
-                    <p className='text-[#556996] max-w-[500px]'>Our process is simple yet effective. Every project is different, but we’ve seen thousands of them since we first launched. Our experience is your asset.</p>
+                    <p className='text-[#556996] max-w-[500px]'>ჩვენი პროცესი მარტივია, მაგრამ ეფექტური. ყველა პროექტი განსხვავებულია, მაგრამ ჩვენ ვნახეთ ათასობით მათგანი, რაც პირველად დავიწყეთ. ჩვენი გამოცდილება თქვენი აქტივია.</p>
 
                 </div>
                 <div className='lg:flex-row flex flex-col items-center  justify-center gap-16 lg:items-start'>
@@ -25,10 +25,10 @@ export default function Process() {
                     </div>
                     <div className='grid grid-cols-1  md:mt-0 md:grid-cols-2 lg:grid-cols-1 gap-12'>
 
-                        <ProcessCard plan='01' title='In-Depth Consultation' description='Carefully assessing your logistics needs to ensure tailored and effective solutions.' />
-                        <ProcessCard plan='02' title='Strategic Planning' description='Designing a best plan to ensure efficient and optimal delivery of your goods.' />
-                        <ProcessCard plan='03' title='Efficient Execution   ' description='Carefully coordinating every detail of the shipment process to ensure smooth and timely execution.' />
-                        <ProcessCard plan='04' title='On-Time Delivery' description='Gaining a deep understanding of your logistics needs to offer the best possible solutions.' />
+                        <ProcessCard plan='01' title='დეტალურუ კონსულტაცია' description=' თქვენი ლოჯისტიკური საჭიროებების ზუსტი შეფასება, რათა უზრუნველვყოთ მორგებული და ეფექტური გადაწყვეტილებები.' />
+                        <ProcessCard plan='02' title='სტრატეგიული დაგეგმვა' description='ეფექტური და ოპტიმალური მიწოდების უზრუნველსაყოფად საუკეთესო გეგმის შედგენა.' />
+                        <ProcessCard plan='03' title='ეფექტური შესრულება' description='თითოეული დეტალის ყურადღებით კოორდინირება, რათა უზრუნველვყოთ სიჩქარე და დროული შესრულება.' />
+                        <ProcessCard plan='04' title='დროული მიწოდება' description='თქვენი ლოჯისტიკური საჭიროებების ღრმა აღქმა საუკეთესო შესაძლო გადაწყვეტილებების შეთავაზებისთვის.' />
 
                     </div>
                 </div>
