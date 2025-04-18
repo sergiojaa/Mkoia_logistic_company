c9fe25c62b307367bc2b8a70b05bfd4e1773199d
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 02:44:13 +0400

    change texts

--- app/components/about/Action.tsx
+++ app/components/about/Action.tsx
@@ -16,18 +16,18 @@ export default function Action() {
     }, [])
 
     return (
-        <div className='w-full mt-10 p-5'>
+        <div className='w-full mt-10 p-7'>
             <div className="flex items-center">
                 <div className="w-4 h-4 rounded-full bg-red-500 mr-2"></div>
-                <p className="text-md uppercase text-[#2A437C] tracking-wider">Gallery</p>
+                <p className="text-md uppercase text-[#2A437C] tracking-wider">გალერეა</p>
             </div>
 
-            <div className='lg:flex lg:w-[1200px] justify-between'>
-                <h3 className='text-[#001E62] max-w-[350px] font-bold text-4xl'>
-                    Fleet in <span className='text-[#D13851]'>Action:</span> Our <span className='text-[#D13851]'>Freight</span> Journey
+            <div className='lg:flex  justify-between'>
+                <h3 className='text-[#001E62] max-w-[450px] font-bold text-4xl'>
+                    მოქმედებაში მოქმედი <span className='text-[#D13851]'>ფლოტი:</span> ჩვენი <span className='text-[#D13851]'>სატვირთო</span> მოგზაურობა
                 </h3>
-                <p className='text-[#556996] text-lg max-w-[550px]'>
-                    Taking you through the journey of our fleet, where every step is carefully managed for precision, speed, and reliability.
+                <p className='text-[#556996] text-lg max-w-[450px]'>
+                    მიგიყვანთ ჩვენი ფლოტის მოგზაურობაში, სადაც ყოველი ნაბიჯი საგულდაგულოდ იმართება სიზუსტისთვის, სიჩქარისა და საიმედოობისთვის.
                 </p>
             </div>
 
