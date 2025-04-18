bca3e4041c7adfc8a88e2ec1f6035dc773e8e975
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 02:33:04 +0400

    change texts

--- app/components/about/Experience.tsx
+++ app/components/about/Experience.tsx
@@ -2,10 +2,10 @@ import ExperienceCards from "./ExperienceCards"
 export default function Experience() {
     return (
         <div className="grid  p-5 grid-cols-1 mt-10 md:grid-cols-2 lg:grid-cols-4 gap-5">
-            <ExperienceCards year="25" text="Years of Industry Experience" />
-            <ExperienceCards year="99%" text="On-Time Delivery Rate" />
-            <ExperienceCards year="500+" text="Satisfied Clients Worldwide" />
-            <ExperienceCards year="300+" text="Employees for Your Success" />
+            <ExperienceCards year="3" text="წლიანი ინდუსტრიის" />
+            <ExperienceCards year="90%" text="მიწოდების მაჩვენებელი" />
+            <ExperienceCards year="500+" text="კმაყოფილი მომხმარებელი" />
+            <ExperienceCards year="10+" text="თანამშრომელი" />
 
         </div>
     )
