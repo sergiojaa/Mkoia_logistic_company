8c54515ba0d487bc7c0907f23774ae1521039f27
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 03:12:58 +0400

    change texts

--- app/components/contact/ContactInfoTexts.tsx
+++ app/components/contact/ContactInfoTexts.tsx
@@ -6,11 +6,11 @@ export default function ContactInfoTexts() {
             <div className='flex flex-col gap-7'>
                 <div className="flex items-center">
                     <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
-                    <p className=" text-[#2A437C]">CONTACT INFO</p>
+                    <p className=" text-[#2A437C]">საკონტაქტო ინფორმაცია</p>
                 </div>
                 <div className='flex flex-col gap-5'>
-                    <h2 className='text-[#001E62]  text-4xl md:text-5xl  max-w-[545px] leading-tight'>Connect <span className='text-[#D13851]'>with Us</span> Today and We will Help Your Business</h2>
-                    <p className='text-[#2A437C] text-[19px] max-w-[595px]'>Ready to take your logistics to the next level? Contact us today to discover how our tailored solutions and  industry expertise can transform your supply chain.</p>
+                    <h2 className='text-[#001E62]  text-4xl   max-w-[545px] leading-tight'>დაგვიკავშირდით და დავეხმარებით  <span className='text-[#D13851]'>თქვენს ბიზნესს</span> </h2>
+                    <p className='text-[#2A437C] text-[19px] max-w-[595px]'>მზად ხართ აიყვანოთ თქვენი ლოჯისტიკა შემდეგ დონეზე? დაგვიკავშირდით დღეს იმის გასარკვევად, თუ როგორ შეუძლია ჩვენი მორგებული გადაწყვეტილებები და ინდუსტრიის გამოცდილება გარდაქმნას თქვენი მიწოდების ჯაჭვი.</p>
                 </div>
             </div>
         </div>
--- app/components/contact/ContactInputs.tsx
+++ app/components/contact/ContactInputs.tsx
@@ -5,60 +5,60 @@ export default function ContactInputs() {
         <div className='mt-10 flex flex-col  items-center justify-center px-4 lg:px-10'>
             <div>
                 <p className='text-[#2A437C] text-2xl  max-w-xl mb-8'>
-                    Please fill out the form below, and <br />
-                    our team will get back to you <br />
-                    promptly.
+                    გთხოვთ შეავსოთ ქვემოთ მოცემული ფორმა და <br />
+                    ჩვენი გუნდი დაგიკავშირდებათ <br />
+                    სასწრაფოდ.
                 </p>
 
                 <form className='grid grid-cols-1 lg:grid-cols-2 gap-6 max-w-3xl'>
                     <div>
-                        <label className='block text-[#001E62] font-medium mb-2'>First Name</label>
+                        <label className='block text-[#001E62] font-medium mb-2'>სახელი</label>
                         <input
                             name='firstname'
-                            placeholder='Enter your first name'
+                            placeholder='სახელი'
                             type="text"
                             className='w-full border border-[#AAB4CB] rounded-md px-4 py-3 text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#3E64AA]'
                         />
                     </div>
 
                     <div>
-                        <label className='block text-[#001E62] font-medium mb-2'>Last Name</label>
+                        <label className='block text-[#001E62] font-medium mb-2'>გვარი</label>
                         <input
                             name='lastName'
-                            placeholder='Enter your last name'
+                            placeholder=' გვარი'
                             type="text"
                             className='w-full border border-[#AAB4CB] rounded-md px-4 py-3 text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#3E64AA]'
                         />
                     </div>
 
                     <div>
-                        <label className='block text-[#001E62] font-medium mb-2'>Email Address</label>
+                        <label className='block text-[#001E62] font-medium mb-2'>ელ-ფოსტა</label>
                         <input
                             name='email'
-                            placeholder='Enter your email address'
+                            placeholder=' ელ-ფოსტა'
                             type="text"
                             className='w-full border border-[#AAB4CB] rounded-md px-4 py-3 text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#3E64AA]'
                         />
                     </div>
 
                     <div>
-                        <label className='block text-[#001E62] font-medium mb-2'>Phone Number</label>
+                        <label className='block text-[#001E62] font-medium mb-2'>ტელეფონის ნომერი</label>
                         <input
                             name='phone'
-                            placeholder='Enter your phone number'
+                            placeholder=' ტელეფონის ნომერი'
                             type="text"
                             className='w-full border border-[#AAB4CB] rounded-md px-4 py-3 text-gray-600 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#3E64AA]'
                         />
                     </div>
                     <div className='md:col-span-2 max-w-[570px]'>
-                        <label className='block text-[#1E2A4A] font-medium mb-2'>Message</label>
+                        <label className='block text-[#1E2A4A] font-medium mb-2'>მესიჯი</label>
                         <textarea
                             name='message'
-                            placeholder='Enter your message'
+                            placeholder='შეიყვანეთ თქვენი მესიჯი'
                             className='w-full border border-[#AAB4CB]  focus:ring-[#3E64AA] placeholder:pl-3 placeholder:pt-3  h-[150px]'
                         ></textarea>
                         <div className='flex items-center justify-center'>
-                            <button className='bg-[#001E62] mt-5 cursor-pointer py-2 rounded-full text-white w-[400px]'>Contact Us</button>
+                            <button className='bg-[#001E62] mt-5 cursor-pointer py-2 rounded-full text-white w-[400px]'>გაგზავნა</button>
 
                         </div>
                     </div>
