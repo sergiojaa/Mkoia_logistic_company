01a0061421fb495876590dc108daaf5cd1132f7b
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 02:47:12 +0400

    change texts

--- app/components/services/Servicesimage.tsx
+++ app/components/services/Servicesimage.tsx
@@ -21,14 +21,13 @@ export default function Servicesimage() {
                     />
                 </div>
                 <div className="absolute inset-0 text-center text-white flex flex-col items-center justify-center px-4">
-                    <h2 className="font-bold text-3xl lg:text-4xl   mb-4">Logistics Services</h2>
+                    <h2 className="font-bold text-3xl lg:text-4xl   mb-4">ლოჯისტიკური სერვისები</h2>
                     <p className="max-w-2xl text-sm md:text-lg mb-6">
-                        Our comprehensive logistics solution is designed to optimize your supply chain and enhance your business
-                        operations.
+                        ჩვენი ყოვლისმომცველი ლოჯისტიკური გადაწყვეტილება შექმნილია თქვენი მიწოდების ჯაჭვის ოპტიმიზაციისა და თქვენი ბიზნეს ოპერაციების გასაუმჯობესებლად.
                     </p>
                     <div className="flex items-center">
                         <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
-                        <p className="text-xs md:text-sm font-medium tracking-wider">CUSTOM SOLUTIONS FOR EVERY NEED</p>
+                        <p className="text-xs md:text-sm font-medium tracking-wider">მორგებული გადაწყვეტილებები ყველა საჭიროებისთვის</p>
                     </div>
                 </div>
             </div>
