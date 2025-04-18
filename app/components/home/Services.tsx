55a25ee313e9511418319ca7f398f9cdbe542098
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 02:14:06 +0400

    change texts

--- app/components/home/Services.tsx
+++ app/components/home/Services.tsx
@@ -5,23 +5,23 @@ export default function Services() {
     return (
         <div className='max-w-[1200px] mt-10 px-7 mx-auto '>
             <div className='flex flex-col items-center justify-between'>
-                <h3>OUR LOGISTIC SERIVCES</h3>
-                <h2 className='text-[#001E62] max-w-[420px] text-center text-[22px]'>Comprehensive <span className='text-[#D13851]'>Logix Services</span>  Tailored to Meet Your Unique <span className='text-[#D13851]'>Needs</span> </h2>
+                <h3>ჩვენი ლოჯისტიკური სერვისი</h3>
+                <h2 className='text-[#001E62] max-w-[420px] text-center text-[22px]'>სრული <span className='text-[#D13851]'>ლოჯისტიკური სერვისები</span>  მორგებული თქვენს უნიკალურ <span className='text-[#D13851]'>საჭიროებებზე</span> </h2>
             </div>
             <div className='flex flex-col gap-10 '>
-                <ServiceCards icon='/home/custom-icon.png' title='Customs Brokerage' description='Navigating customs with ease, ensuring your goods clear borders swiftly and compliantly.' />
-                <ServiceCards icon='/home/custom-icon.png' title='Warehousing & Distribution'
-                    description='Secure storage and efficient distribution solutions to keep your inventory flowing smoothly.' />
+                <ServiceCards icon='/home/custom-icon.png' title='საბაჟრო ბროკერობა' description='საბაჟოზე მარტივი ნავიგაცია, თქვენი საქონლის საზღვრების სწრაფად და შესაბამისობის უზრუნველყოფა..' />
+                <ServiceCards icon='/home/custom-icon.png' title='საწყობი & დისტრიბუცია'
+                    description='უსაფრთხო შენახვისა და ეფექტური განაწილების გადაწყვეტილებები თქვენი ინვენტარის შეუფერხებლად მიმდინარეობის შესანარჩუნებლად..' />
                 <ServiceCards
                     icon='/home/third-icon.png'
-                    title='Supply Chain Management'
-                    description='Navigating customs with ease, ensuring your goods clear borders swiftly and compliantly.' />
-                <ServiceCards icon='/home/fourth-icon.png' title='Cross-Border Solutions'
-                    description='Seamless cross-border logistics to connect your business with international markets.' />
-                <ServiceCards icon='/home/fifth-icon.png' title='Last-Mile Delivery'
-                    description='Reliable last-mile delivery that gets your products to customers’ doorsteps with precision.' />
-                <ServiceCards icon='/home/sixth-icon.png' title='Project Cargo Handling'
-                    description='Specialized handling for oversized or complex shipments, with tailored logistics solutions.' />
+                    title='მიწოდების ჯაჭვის ელემენტი'
+                    description='საბაჟო პროცესების ეფექტურად მართვა, რათა თქვენი ტვირთი სწრაფად და კანონიერად გადმოსცდეს საზღვრებს' />
+                <ServiceCards icon='/home/fourth-icon.png' title='საზღვრის გადმოკვეთის გადაწყვეტილებები'
+                    description='დროული საზღვრის გადმოკვეთა რათა მალე მოხდეს თქვენი ბიზნესის საერთაშორისო ბაზარზე დაკავშირება' />
+                <ServiceCards icon='/home/fifth-icon.png' title='ზუსტ წერტილამდე მოტანა'
+                    description='სანდო ზუსტ წერტილამდე მოტანა, რაც თქვენ გიხსნით კიდევ სხვა პასუხისმგებლობებს' />
+                <ServiceCards icon='/home/sixth-icon.png' title='ტვირთის მართვა'
+                    description='სპეციალიზებული დამუშავება დიდი ზომის ან რთული გადაზიდვებისთვის, მორგებული ლოგისტიკური გადაწყვეტილებებით.' />
 
             </div>
 
