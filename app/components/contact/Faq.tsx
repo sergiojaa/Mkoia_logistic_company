c2cc5542bdf42c99a22fa8ab130d4ce3e0729b56
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 03:18:50 +0400

    change texts

--- app/components/contact/Faq.tsx
+++ app/components/contact/Faq.tsx
@@ -11,10 +11,10 @@ export default function Faq() {
 
                 <div className="flex  items-center">
                     <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
-                    <p className=" text-[#2A437C] text-md   ">FREQUENTLY ASK QUESTION</p>
+                    <p className=" text-[#2A437C] text-md   ">ხშირად დასმული კითხვები</p>
                 </div>
                 <div className='flex flex-col items-center gap-6'>
-                    <p className='text-[#001E62] text-2xl lg:text-3xl mx-auto max-w-[450px] text-center'>Comprehensive <span className='text-[#D13851]'>Questions</span> About Our Services </p>
+                    <p className='text-[#001E62] text-2xl lg:text-3xl mx-auto max-w-[450px] text-center'>ყოვლისმომცველი <span className='text-[#D13851]'>კითხვები</span> ჩვენი სერვისების შესახებ </p>
                 </div>
 
             </div>
