d60a83cc54b013ba0ce8cb6023de682a0300d265
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 03:33:01 +0400

    change text

--- app/components/about/Experience.tsx
+++ app/components/about/Experience.tsx
@@ -2,7 +2,7 @@ import ExperienceCards from "./ExperienceCards"
 export default function Experience() {
     return (
         <div className="grid  p-5 grid-cols-1 mt-10 md:grid-cols-2 lg:grid-cols-4 gap-5">
-            <ExperienceCards year="3" text="წლიანი ინდუსტრიის" />
+            <ExperienceCards year="3" text="წლიანი გამოცდილება" />
             <ExperienceCards year="90%" text="მიწოდების მაჩვენებელი" />
             <ExperienceCards year="500+" text="კმაყოფილი მომხმარებელი" />
             <ExperienceCards year="10+" text="თანამშრომელი" />
