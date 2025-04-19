690e179d22c9004f9a40ef24c358e30557bdcede
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 21:22:45 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.progressprofiles.com", 't3.ftcdn.net', '1000logos.net', 'i0.wp.com', 'www.progressprofiles.com',
-      'www.decksdirect.com'],
+    domains: [
+      "www.progressprofiles.com",
+      't3.ftcdn.net',
+      '1000logos.net',
+      'i0.wp.com',
+      'www.progressprofiles.com',
+      'www.decksdirect.com'
+    ],
   },
+  output: 'export',
 };
 
 export default nextConfig;
