afca1702904e5fbf9a31fd1e26ea8ef0d3a22435
seriojaa <kaliashvilisergi@gmail.com>
2025-04-19 19:34:48 +0400

    add new image

--- app/components/about/Whoarewe.tsx
+++ app/components/about/Whoarewe.tsx
@@ -16,24 +16,24 @@ export default function Whoarewe() {
                         <div className="w-3 h-3 rounded-full bg-red-500 mr-2"></div>
                         <h3>ვინ ვართ ჩვენ</h3>
                     </div>
-                    <h2 className='text-[#001E62] max-w-[520px] text-4xl'>ჩვენ ვსპეციალიზირდებით <span className='text-[#D13851]'> მახალი ხარისხის ლოჯისტიკურ სერვისში</span> რომელიც გპირდებათ დროულ და უსაფრთხო მიწოდებას</h2>
+                    <h2 className='text-[#001E62] max-w-[520px] text-4xl'>ჩვენ ვსპეციალიზირდებით <span className='text-[#D13851]'> მახალი ხარისხის სამშენებლო და სარემონტო მასალების  </span> მისოწების სერვისში </h2>
                     <p className='text-[#2A437C] max-w-[550px] text-md '>
-                        ჩვენი ერთგული გუნდი მუშაობს უწყვეტ რეჟიმში, რათა უზრუნველყოს მორგებული გადაწყვეტილებები ყველა ზომის ბიზნესისთვის, რაც გეხმარებათ მიზნების მარტივად და სანდრო შესრულებაში. საბაჟო ბროკერობიდან დაწყებული, ბოლო კილომეტრის მიწოდებამდე, ჩვენ ვალდებულნი ვართ ვუზრუნველყოთ მაღალი ხარისხი და სიზუსტე თითოეულ სერვისში
+                        ჩვენი ერთგული გუნდი მუშაობს უწყვეტ რეჟიმში, რათა უზრუნველყოს მორგებული გადაწყვეტილებები ყველა ზომის ბიზნესისთვის, რაც გეხმარებათ მიზნების მარტივად და სანდო შესრულებაში.
                     </p>
                     <div className='max-w-[490px] border-b-4 border-b-[#D13851] rounded-xl p-5 bg-[#EEF4FF]'>
                         <h2 className='text-[#001E62] font-bold'>
                             ჩვენ ვამარტივებთ ლოჯისტიკას მოწინავე ტექნოლოგიით, ასე რომ თქვენ შეგიძლიათ ფოკუსირება მოახდინოთ ბიზნესის ზრდაზე                        </h2>
                     </div>
                 </div>
-                <div className="relative w-full max-w-[400px]">
+                <div className=" w-full max-w-[400px]">
                     <Image
-                        src="/about/containers.png"
+                        src="https://i0.wp.com/www.buildingcode.co.ke/wp-content/uploads/2020/11/Building-Supplies.png?fit=740%2C485&ssl=1"
                         alt="logistic containers"
                         width={400}
                         height={300}
                         className="w-full h-auto rounded-lg"
                     />
-                    <div className="absolute bottom-5 left-5  bg-white rounded-xl shadow-md p-6 w-[70%] max-w-xs space-y-4">
+                    <div className=" bottom-5 left-5  bg-white rounded-xl shadow-md p-6 w-[70%] max-w-xs space-y-4">
                         {values.map(({ icon, label }) => (
                             <div key={label} className="flex items-center gap-3 text-[#001E62] font-medium text-lg">
                                 {icon}
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.progressprofiles.com", 't3.ftcdn.net', '1000logos.net'],
+    domains: ["www.progressprofiles.com", 't3.ftcdn.net', '1000logos.net', 'i0.wp.com'],
   },
 };
 
