17c2c3167ea966631a3c833fde8d766d59d3b041
seriojaa <kaliashvilisergi@gmail.com>
2025-04-20 13:50:06 +0400

    change pplaces for icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,21 +9,19 @@ export default function Footer() {
             <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between gap-10">
                 {/* Left - Logo & Text */}
                 <div className="lg:w-1/3 space-y-6">
-                    <h1 className="text-4xl font-bold">
-                        LOG<span className="text-red-500">I</span>
-                        <span className="text-blue-500">X</span>
-                    </h1>
-                    <p className="text-sm text-gray-300">
-                        Join our community to unlock exclusive insights and innovative solutions for all your logistics needs
+                    <h1 className="text-4xl text-white">M<span className="text-[#D13851]">K</span>O<span className="text-[#D13851]">I</span>A</h1>
+
+                    <p className="text-md text-gray-300">
+                        გამოგვყევი სოციალურ პლატმორმებზე რათა მიიღო ინფორმაცია პირველმა
                     </p>
 
                     {/* Social Icons */}
                     <div className="flex items-center gap-4 mt-4">
-                        <a href="#" aria-label="Instagram">
-                            <Instagram />
-                        </a>
                         <a href="#" aria-label="Facebook">
                             <Facebook />
+                        </a>
+                        <a href="#" aria-label="Instagram">
+                            <Instagram />
                         </a>
                         <a href="#" aria-label="WhatsApp">
                             <FontAwesomeIcon icon={faWhatsapp} className="  w-7 h-6" />
@@ -34,14 +32,14 @@ export default function Footer() {
                 {/* Middle - Headline */}
                 <div className="lg:w-1/3 flex items-start lg:items-center">
                     <h2 className="text-xl font-semibold leading-snug">
-                        The Future of Urban<br />Delivery Starts Here
+                        თქვენი მიზნების ასრულება<br />იწყება აქ
                     </h2>
                 </div>
 
                 {/* Right - Company Links */}
                 <div className="lg:w-1/3">
-                    <h3 className="font-semibold mb-3">Company</h3>
-                    <ul className="space-y-2 text-gray-300 text-sm">
+                    <h3 className="text-xl  mb-3">კომპანია</h3>
+                    <ul className="space-y-2 text-gray-300 text-md">
                         <Link href="/about">
                             <li>About</li>
                         </Link>
