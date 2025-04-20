f9996d961860c2e02d13da9bc5a567ff29da1075
seriojaa <kaliashvilisergi@gmail.com>
2025-04-20 13:07:28 +0400

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Provide from "./components/home/Provide";
 import Services from "./components/home/Services";
 // import Trackway from "./components/home/Trackway";
-import Transit from "./components/home/Transit";
+// import Transit from "./components/home/Transit";
 import Trust from "./components/home/Trust";
 import ValueCard from "./components/home/ValueCard";
 
