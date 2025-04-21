dafa325b1869696a269d958dcd84e09f5dc5b086
seriojaa <kaliashvilisergi@gmail.com>
2025-04-21 15:12:57 +0400

    add call function on number

--- app/components/contact/ContactCards.tsx
+++ app/components/contact/ContactCards.tsx
@@ -1,10 +1,12 @@
 import { LucideIcon } from 'lucide-react';
-import React from 'react'
+import React from 'react';
+
 interface ContactCard {
     Icon: LucideIcon;
     title: string;
     description: string;
 }
+
 export default function ContactCards({ Icon, title, description }: ContactCard) {
     return (
         <div className="rounded-3xl max-w-[350px] border border-[#E5E7EB] bg-white p-6 shadow-sm">
@@ -12,11 +14,15 @@ export default function ContactCards({ Icon, title, description }: ContactCard) 
                 <div className="flex h-12 w-12 items-center justify-center rounded-full bg-[#CCD2DF]">
                     <Icon className="h-5 w-5 text-[#001E62]" />
                 </div>
-                <div className="text-lg font-medium  text-[#556996]">{title}</div>
+                <div className="text-lg font-medium text-[#556996]">{title}</div>
             </div>
             <div>
-                <p className="text-xl  text-[#001E62]">{description}</p>
+                {title === 'ტელეფონი' ? (
+                    <a href={`tel:${description}`} className="text-xl text-[#001E62]">{description}</a>
+                ) : (
+                    <p className="text-xl text-[#001E62]">{description}</p>
+                )}
             </div>
         </div>
-    )
+    );
 }
