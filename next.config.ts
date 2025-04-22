b7bca8cb9fa305b594ca9dc7ee27748192761464
seriojaa <kaliashvilisergi@gmail.com>
2025-04-22 21:47:59 +0400

    add new images update next config

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,9 @@ const nextConfig: NextConfig = {
       '1000logos.net',
       'i0.wp.com',
       'www.progressprofiles.com',
-      'www.decksdirect.com'
+      'www.decksdirect.com',
+      "newlight.ge",
+      "www.bautech.ge"
     ],
   },
 };
