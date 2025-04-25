df71fd7b3af2472e29da679d3ec766e890a34ed7
seriojaa <kaliashvilisergi@gmail.com>
2025-04-25 20:18:38 +0400

    Add english translation

--- app/components/services/Service.tsx
+++ app/components/services/Service.tsx
@@ -1,19 +1,23 @@
+'use client'
 import React from 'react'
 import ServiceCards from './ServiceCards'
 import { SquareUser } from 'lucide-react'
+import { useTranslate } from '@/app/utils/translate'
 
 export default function Service() {
+    const t = useTranslate()
+
     return (
         <div className='w-full mx-auto mt-10 px-7 '>
             <div className='flex  flex-col gap-7 justify-center items-center'>
                 <div className="flex  items-center">
                     <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
-                    <p className=" text-[#2A437C] text-md   ">ლოჯისტიკური სერვისები</p>
+                    <p className=" text-[#2A437C] text-md   ">{t('logistics-services')}</p>
                 </div>
                 <div className='flex flex-col items-center gap-6'>
-                    <p className='text-[#001E62] text-xl lg:text-2xl mx-auto max-w-[510px] text-center'>ყოვლისმომცველი <span className='text-[#D13851]'>ლოჯისტიკური სერვისები</span>  რომლებიც მორგებულია თქვენს  <span className='text-[#D13851]'>უნიკალურ საჭიროებებზე</span> </p>
+                    <p className='text-[#001E62] text-xl lg:text-2xl mx-auto max-w-[510px] text-center'>{t('comprehensive')} <span className='text-[#D13851]'>{t('logistics-services')}</span>  {t('tailore')} <span className='text-[#D13851]'>{t('unique-needs')}</span> </p>
                     <p className='text-[#2A437C] text-center mx-auto max-w-[800px]'>
-                        ჩვენი მიზანია ვიყოთ წამყვანი ლოგისტიკური კომპანია სამშენებლო განხრით</p>
+                        {t('our-goal')}</p>
                 </div>
             </div>
             <div className='grid mt-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
