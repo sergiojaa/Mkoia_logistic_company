ef978acfc71b15f59c4e3ea852b7923656786cc4
seriojaa <kaliashvilisergi@gmail.com>
2025-04-25 20:05:51 +0400

    add english translation

--- app/components/services/Community.tsx
+++ app/components/services/Community.tsx
@@ -1,15 +1,19 @@
+'use client'
 import { ArrowRight, } from 'lucide-react'
 import React from 'react'
 import ExperienceCards from './ExperienceCards'
+import { useTranslate } from '@/app/utils/translate'
 
 export default function Community() {
+    const t = useTranslate()
+
     return (
         <div className='w-full  p-7 '>
             <div className='lg:flex-row flex flex-col  gap-5'>
                 <div className='flex flex-col mt-8 gap-6'>
                     <div className="flex items-center">
                         <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
-                        <p className=" text-[#2A437C]   ">ჩვენი საზოგადოება</p>
+                        <p className=" text-[#2A437C]   ">{t('our-community')}</p>
                     </div>
                     <div className='flex flex-col gap-15'>
                         <h2 className='text-[#001E62] max-w-[700px] text-4xl md:text-4xl font-bold leading-tight'>შემოუერთდი <span className='text-[#D13851]'>მზარდი <br /> ბიზნესისა</span> და ლოჯისტიკის პარტნიორებს </h2>
--- app/components/services/Servicesimage.tsx
+++ app/components/services/Servicesimage.tsx
@@ -27,11 +27,11 @@ export default function Servicesimage() {
                 <div className="absolute inset-0 text-center text-white flex flex-col items-center justify-center px-4">
                     <h2 className="font-bold text-2xl lg:text-3xl   mb-4">{t("services")}</h2>
                     <p className="max-w-2xl text-sm md:text-lg mb-6">
-                        ჩვენი ყოვლისმომცველი ლოჯისტიკური გადაწყვეტილება შექმნილია თქვენი პროექტები ოპტიმიზაციისა და თქვენი მიზნების გასაუმჯობესებლად.
+                        {t('our-decision')}
                     </p>
                     <div className="flex items-center">
                         <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
-                        <p className="text-xs md:text-sm font-medium tracking-wider">მორგებული გადაწყვეტილებები ყველა საჭიროებისთვის</p>
+                        <p className="text-xs md:text-sm font-medium tracking-wider">{t('adjust')}</p>
                     </div>
                 </div>
             </div>
