196c7ed520e438a29624cc04d53bb774947fc644
seriojaa <kaliashvilisergi@gmail.com>
2025-04-25 20:22:16 +0400

    Add english translation

--- app/components/services/Process.tsx
+++ app/components/services/Process.tsx
@@ -1,20 +1,24 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import ProcessCard from './ProcessCard'
+import { useTranslate } from '@/app/utils/translate'
 export default function Process() {
+    const t = useTranslate()
+
     return (
         <div className='w-full mt-10  mx-auto px-7'>
             <div className='flex lg:items-start  flex-col gap-7 justify-center items-center'>
                 <div className="flex   items-center">
                     <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
-                    <p className=" text-[#2A437C] text-xl lg:text-2xl  ">სამუშაო პროცესი</p>
+                    <p className=" text-[#2A437C] text-xl lg:text-2xl  ">{t('work-process')}</p>
                 </div>
                 {/* <div >
                     <h2 className='text-[#001E62] text-2xl lg:text-3xl mx-auto max-w-[500px] '>ჩვენი <span className='text-[#D13851]'>საუკეთესო პროცესი </span> სრულყოფილებისთვის</h2>
 
                 </div> */}
                 <div>
-                    <p className='text-[#556996] lg:text-xl max-w-[500px]'>ჩვენი პროცესი მარტივია, მაგრამ ეფექტური. თითოეული პროექტი უნიკალურია, თუმცა ჩვენ ვასრულებთ ყველას.</p>
+                    <p className='text-[#556996] lg:text-xl max-w-[500px]'>{t("our-work-easy")}</p>
 
                 </div>
                 <div className='lg:flex-row flex flex-col items-center  justify-center gap-16 lg:items-start'>
