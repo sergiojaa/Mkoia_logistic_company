886cdc93a81aedd072c072b3c627119138386383
seriojaa <kaliashvilisergi@gmail.com>
2025-04-28 19:11:50 +0400

    fix size

--- app/components/services/Community.tsx
+++ app/components/services/Community.tsx
@@ -9,14 +9,14 @@ export default function Community() {
 
     return (
         <div className='w-full  p-7 '>
-            <div className='lg:flex-row flex flex-col  gap-5'>
+            <div className='lg:flex-row lg:justify-between flex flex-col  gap-5'>
                 <div className='flex flex-col mt-8 gap-6'>
                     <div className="flex items-center">
                         <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-red-500 mr-2"></div>
                         <p className=" text-[#2A437C]   ">{t('our-community')}</p>
                     </div>
                     <div className='flex flex-col gap-15'>
-                        <h2 className='text-[#001E62] max-w-[700px] text-xl  lg:text-3xl  leading-tight'>{t('join')} <span className='text-[#D13851]'>{t('grow')} <br /> {t('business')}</span> {t('logistic-partners')}</h2>
+                        <h2 className='text-[#001E62] max-w-[700px] text-xl  lg:text-3xl  leading-tight'>{t('join')} <span className='text-[#D13851]'>{t('grow')}  {t('business')}</span> <br /> {t('logistic-partners')}</h2>
                         {/* <p className='text-[#2A437C] text-[19px] max-w-[595px]'>შეუერთდით ბიზნესისა და ლოჯისტიკური პარტნიორების დინამიურ საზოგადოებას.მომარაგების ჯაჭვების ოპტიმიზაცია და ზრდის სტიმულირება </p> */}
                         <Link href={'/contact'}>
                             <button className="flex max-w-max hover:bg-[#001E62] hover:text-white cursor-pointer items-center gap-3 rounded-full border border-[#001E62] text-[#001E62] px-6 py-2 transition-colors hover:border-[#3366CC]">
