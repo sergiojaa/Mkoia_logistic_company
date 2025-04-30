913843d0f7a3ac3f24fa6694ab959c695f76b889
seriojaa <kaliashvilisergi@gmail.com>
2025-04-30 14:22:18 +0400

    fix erors

--- app/components/contact/ContactInputs.tsx
+++ app/components/contact/ContactInputs.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useTranslate } from '@/app/utils/translate'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 export default function ContactInputs() {
     const t = useTranslate()
